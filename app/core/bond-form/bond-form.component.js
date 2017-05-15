44e05a6d35b9798582268017545d8660eca54c3d
Dan Conway <surreal7@gmail.com>
2017-05-15 18:10:10 -0500

    only allow bond if under limit

--- app/core/bond-form/bond-form.component.js
+++ app/core/bond-form/bond-form.component.js
@@ -16,13 +16,17 @@
           if( (parseInt(this.multiplier) * vm.ebsVars.price) > parseInt(this.account.bondBalance)){
             growl.error('Insufficient Funds: '+(vm.multiplier*vm.ebsVars.price)+' EXP is required to be deposited into the EBS contract wallet by account '+this.account.address+'. Current EBS Wallet Balance is '+vm.account.bondBalance, {title:"Insufficient Funds", ttl: -1});
           } else {
-            bondService.confirmModal(
-              "Confirm Bond Purchase",
-              "You are about to purchase a bond with a multiplier of " + vm.multiplier + " for a total of " + (vm.multiplier * vm.ebsVars.price) + "EXP.<br />Are you sure you wish to proceed?",
-              function() { 
-                bondService.unlockedCall(vm.account.address, function() { bondService.buyBond(vm.multiplier, vm.account.address); });
-              }
-            );
+            if(parseInt(vm.multiplier) <= vm.ebsVars.bondsAvail){
+              bondService.confirmModal(
+                "Confirm Bond Purchase",
+                "You are about to purchase a bond with a multiplier of " + vm.multiplier + " for a total of " + (vm.multiplier * vm.ebsVars.price) + "EXP.<br />Are you sure you wish to proceed?",
+                function() { 
+                  bondService.unlockedCall(vm.account.address, function() { bondService.buyBond(vm.multiplier, vm.account.address); });
+                }
+              );
+            } else {
+               growl.error('We are sorry, there is no longer enough available bonds to complete your request. We will announce when more bonds will be made available.', {title:"Bonds Unavailable", ttl: -1});
+            }
           }
         } else {
           growl.error('Multiplier value must be a valid whole number more than zero.', {title:"Bond Purchase Error", ttl: -1});
